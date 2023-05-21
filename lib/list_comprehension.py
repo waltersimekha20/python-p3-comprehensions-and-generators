b2ec3012e6870146acd47d17280cc3ac76b2f33d
waltersimekha <waltersimekha@gmail.com>
2023-05-21 16:10:21 +0300

    completed assignment

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
+    
+    evens = [num for num in num_list if num % 2 == 0]
+    return evens
+
     pass
 
 def make_exclamation(sentence_list):
+ 
+    exclamation_sentences = [sentence + "!" for sentence in sentence_list]
+    return exclamation_sentences
+
     pass
